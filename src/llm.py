c2c81d28feb926aa4820557c148d108a5173879a
Shrabani_Das <das.shrabani2024@gmail.com>
2026-06-28 12:34:47 +0530

    LLM Change

--- src/llm.py
+++ src/llm.py
@@ -4,6 +4,8 @@ import streamlit as st
 
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+
 
 load_dotenv()
 
@@ -22,3 +24,50 @@ def load_llm():
     )
 
     return llm
+
+
+
+def build_prompt():
+
+    return ChatPromptTemplate.from_template(
+        """
+You are an expert Basel III regulatory assistant.
+
+Rules
+
+1. Answer ONLY using the provided context.
+
+2. Never use outside knowledge.
+
+3. If the answer is unavailable reply:
+
+"I don't have enough information in the provided Basel document."
+
+Context
+
+{context}
+
+Question
+
+{question}
+
+Answer
+"""
+    )
+
+
+def generate_answer(
+    llm,
+    prompt,
+    context,
+    question
+):
+
+    formatted_prompt = prompt.format(
+        context=context,
+        question=question
+    )
+
+    response = llm.invoke(formatted_prompt)
+
+    return response.content
